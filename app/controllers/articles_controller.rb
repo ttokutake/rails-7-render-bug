b96c3acec49e61d72709e7e549dec6728c028f9b
ttokutake <tadatoshi.tokutake@quipper.com>
2022-09-22 16:06:20 +0900

    Use respond_to for rendering

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
   def foo
+    respond_to do |format|
+      format.json { render(json: {message: 'hello!'}) }
+      format.all { render(template: 'articles/foo') }
+    end
   end
 end
